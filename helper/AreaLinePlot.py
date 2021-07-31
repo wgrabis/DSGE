927b96ee3b0c916743423c52cde5381f961d3686
wgrabis <wgrabis@gmail.com>
2021-08-01 01:55:10 +0200

    Improved plots

--- helper/AreaLinePlot.py
+++ helper/AreaLinePlot.py
@@ -15,9 +15,22 @@ class AreaLinePlot(BasePlot):
         # todo bugged
         # axs.fill_between(self.data_x, self.bottom, self.top)
         axs.plot(self.data_x, self.average, color='r')
+        axs.plot(self.data_x, [0 for _ in self.data_x], color='b')
         # axs.figure(figsize=(3, 3))
-
         # axs..set_aspect('equal')
+
+        max_y = max(self.average)
+        min_y = min(self.average)
+
+        max_y = max(0, max_y)
+        min_y = min(0, min_y)
+
+        if min_y < 0 and max_y > 0:
+            max_y = max(abs(min_y), max_y)
+            min_y = -1 * max(abs(min_y), max_y)
+
+        axs.set_ylim([min_y * 1.1, max_y * 1.1])
+        axs.set_xlim([0, max(self.data_x)])
 
         axs.set_title(self.name)
         axs.set(xlabel=self.x_name, ylabel=self.y_name)
